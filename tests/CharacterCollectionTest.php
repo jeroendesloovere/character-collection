31b8b7add8c135fc77997c05da13d62d0fe82908
Jeroen Desloovere <info@jeroendesloovere.be>
2018-04-03 11:23:38 +0200

    Use latest phpunit + minimum php version 7.1

--- tests/CharacterCollectionTest.php
+++ tests/CharacterCollectionTest.php
@@ -2,34 +2,44 @@
 
 namespace JeroenDesloovere\CharacterCollection;
 
-// required to load
-require_once __DIR__ . '/../vendor/autoload.php';
+use PHPUnit\Framework\TestCase;
 
-class CharacterCollectionTest extends \PHPUnit_Framework_TestCase
+class CharacterCollectionTest extends TestCase
 {
+    /** @var CharacterCollection */
+    private $correctResult;
+
+    /** @var string */
+    private $sentence;
+    
+    public function setUp()
+    {
+        $this->sentence = 'this is a test';
+
+        $this->correctResult = new CharacterCollection('');
+        $this->correctResult->add(0, 't');
+        $this->correctResult->add(1, 'h');
+        $this->correctResult->add(2, 'i');
+        $this->correctResult->add(3, 's');
+        $this->correctResult->add(4, ' ');
+        $this->correctResult->add(5, 'i');
+        $this->correctResult->add(6, 's');
+        $this->correctResult->add(7, ' ');
+        $this->correctResult->add(8, 'a');
+        $this->correctResult->add(9, ' ');
+        $this->correctResult->add(10, 't');
+        $this->correctResult->add(11, 'e');
+        $this->correctResult->add(12, 's');
+        $this->correctResult->add(13, 't');
+    }
+    
     public function testCharacters()
     {
-        $sentence = 'this is a test';
+        $charactersCollection = new CharacterCollection($this->sentence);
 
-        $correctResult = new CharacterCollection('');
-        $correctResult->add(0, 't');
-        $correctResult->add(1, 'h');
-        $correctResult->add(2, 'i');
-        $correctResult->add(3, 's');
-        $correctResult->add(4, ' ');
-        $correctResult->add(5, 'i');
-        $correctResult->add(6, 's');
-        $correctResult->add(7, ' ');
-        $correctResult->add(8, 'a');
-        $correctResult->add(9, ' ');
-        $correctResult->add(10, 't');
-        $correctResult->add(11, 'e');
-        $correctResult->add(12, 's');
-        $correctResult->add(13, 't');
+        $this->assertEquals($this->correctResult, $charactersCollection);
+        $this->assertEquals($this->sentence, (string) $charactersCollection);
 
-        $charactersCollection = new CharacterCollection($sentence);
-
-        $this->assertEquals($correctResult, $charactersCollection);
-        $this->assertEquals($sentence, (string) $charactersCollection);
+        $this->assertEquals([0, 10, 13], $charactersCollection->get('t')->getPositions());
     }
 }
